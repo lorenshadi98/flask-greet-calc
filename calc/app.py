475e4dd1b8333b8449bd05dabca391c9ce150793
lorenshadi98 <lorans.hadi@gmail.com>
2021-08-27 18:02:14 -0700

    Completed Flask and Calc

---
+++ calc/app.py
@@ -0,0 +1,73 @@
+
+from flask import Flask
+from flask.wrappers import Request
+from werkzeug.wrappers import request
+from operations import add, sub, mult, div
+
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def add_operation():
+    """Add a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+
+@app.route('/sub')
+def sub_operation():
+    """Subtract a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+
+@app.route('/mult')
+def mult_operation():
+    """Multiply a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+
+@app.route('/div')
+def divide_operation():
+    """Divide a and b parameters."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+
+
+# PART TWO
+
+operators = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+
+@app.route("/math/<oper>")
+def do_math(oper):
+    """Do math on a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = operators[oper](a, b)
+
+    return str(result)
